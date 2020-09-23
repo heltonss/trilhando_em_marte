a5dbf5e05a0fc5f0ab2748dc4f21e05d59ed91dc
Pedro Henrique Soares de Cam | Cappta <pedro.soares@cappta.com.br>
2020-09-23 09:31:30 -0300

    Adicionado tratativa de erro para posição final da Sonda negativa

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -272,7 +272,7 @@ function saidaPosicaoSonda() {
         alert("Insira a marcação inicial da malha, a posição inicial da Sonda e seus comandos!")
     } else if (xMalha == null || yMalha == null) {
         alert("Defina o tamanho da malha a ser explorada!")
-    } else if (xSonda > xMalha || ySonda > yMalha) {
+    } else if (xSonda > xMalha || ySonda > yMalha || xSonda < xMalha || ySonda < yMalha) {
         alert("Você mandou a sonda para fora de Marte :(")
     } else {
         posicaoFinal.innerHTML = `${xSonda} ${ySonda} ${direcaoSonda}`
